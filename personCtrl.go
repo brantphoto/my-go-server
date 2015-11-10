d2a8156f13b1276fb308090c856fbb6e45032e2b
brantphoto <brant.barger@gmail.com>
2015-11-10 15:57:32 -0800

    queries for people type

--- personCtrl.go
+++ personCtrl.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	//"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
@@ -10,14 +12,21 @@ import (
 //ROOT CONTROLLERS
 func PersonIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 
 	sessionCopy := session.Copy()
 	defer sessionCopy.Close()
 
 	c := sessionCopy.DB(dbName).C("people")
 	result := People{}
-	c.Find(bson.M{"name": "Ale"}).All(&result)
+
+	switch r.FormValue("name") {
+	case "":
+		fmt.Println("hi!")
+		c.Find(bson.M{}).All(&result)
+	default:
+		fmt.Println("hi!")
+		c.Find(bson.M{"name": r.FormValue("name")}).All(&result)
+	}
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(result); err != nil {
 		panic(err)
